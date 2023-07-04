c7321627f5c4243921ff6854f6f14fa060e9f179
PedroAC18 <pedro.correa1101@gmail.com>
2023-07-04 18:24:44 -0300

    DepartmentDAO changes

--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,10 +1,15 @@
 package dao;
 
+import dao.impl.DepartmentDaoJDBC;
 import dao.impl.SellerDaoJDBC;
 import db.DB;
 
 public class DaoFactory {
     public static SellerDAO createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());//faz a conexão com o BD do MySQL
+    }
+
+    public static DepartmentDAO createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
