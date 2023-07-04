59c8a1264f366ecee8675c097fae4a167d10bc24
PedroAC18 <pedro.correa1101@gmail.com>
2023-07-04 17:56:30 -0300

    Insert, update and delete methods

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import dao.SellerDAO;
 import entities.Department;
 import entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -27,6 +28,23 @@ public class Program {
         for(Seller obj: list){
             System.out.println(obj);
         }
+        System.out.println();
+        System.out.println("=== TEST 4: Seller insert===");
+        Seller newSeller = new Seller(null, "Sara", "sara@gmail.com", new Date(), 5000.0, dep);
+        sellerDAO.insert(newSeller);
+        System.out.println("ID novo seller" + newSeller.getId());
+
+        System.out.println();
+        System.out.println("=== TEST 5: Seller insert===");
+        seller = sellerDAO.findById(1);
+        seller.setName("João");
+        sellerDAO.update(seller);
+        System.out.println("Update done");
+
+        System.out.println();
+        System.out.println("=== TEST 6: Seller delete===");
+        sellerDAO.deleteById(5);
+        System.out.println("Seller deleted");
 
     }
 }
--- src/dao/impl/SellerDaoJDBC.java
+++ src/dao/impl/SellerDaoJDBC.java
@@ -7,10 +7,7 @@ import db.DbIntegrityException;
 import entities.Department;
 import entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,17 +23,79 @@ public class SellerDaoJDBC implements SellerDAO {
 
     @Override
     public void insert(Seller obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "INSERT INTO seller\n" +
+                    "(Name, Email, BirthDate, BaseSalary, DepartmentId) \n" +
+                    "VALUES \n" +
+                    "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1,obj.getName());
+            st.setString(2,obj.getEmail());
+            st.setDate(3,new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4,obj.getBaseSalary());
+            st.setInt(5,obj.getDepartment().getId());
 
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected>0){
+                ResultSet rs = st.getGeneratedKeys();
+                if(rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setId(id);//associando o id ao novo objeto instanciado
+                }
+                DB.closeResultSet(rs);
+            }else{
+                throw new DbException("Erro inesperado");
+            }
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public void update(Seller obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "UPDATE seller \n" +
+                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? \n" +
+                    "WHERE Id = ?");
+            st.setString(1,obj.getName());
+            st.setString(2,obj.getEmail());
+            st.setDate(3,new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4,obj.getBaseSalary());
+            st.setInt(5,obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "DELETE FROM seller \n" +
+                    "WHERE Id = ?");
 
+            st.setInt(1,id);
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
